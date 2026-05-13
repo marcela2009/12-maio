6c6b9076870d69ff12783d7c0da3e6a26106c078
marcela2009 <marcela.franca.silva@escola.pr.gov.br>
2026-05-13 08:41:58 -0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const botoes = document.querySelectorAll(".botao");
-
+console.log(botoes);
 for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-    }
-
+    console.log(i);
+}
+for (let i = 0; i < botoes.length; i++) {
+    console.log(i);
+}
+botoes[i].onclick = function () { };
+botoes[i].onclick = function () {
     botoes[i].classList.add("ativo");
-  };
+};
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        botoes[i].classList.add("ativo");
+    };
+}
+for (let j = 0; j < botoes.length; j++) {
+    botoes[j].classList.remove("ativo");
 }
