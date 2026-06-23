b7779c3e319c3f02cfb831aaf8d935ca7384103b
marcela2009 <marcela.franca.silva@escola.pr.gov.br>
2026-06-23 09:30:55 -0300

    .

--- main.js
+++ main.js
@@ -1,32 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
-for (let i = 0; i < botoes.length; i++) {
-    console.log(i);
-}
-for (let i = 0; i < botoes.length; i++) {
-    console.log(i);
-}
-botoes[i].onclick = function () { };
-botoes[i].onclick = function () {
-    botoes[i].classList.add("ativo");
-};
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        botoes[i].classList.add("ativo");
-    };
-}
-for (let j = 0; j < botoes.length; j++) {
-    botoes[j].classList.remove("ativo");
-}
-
-const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-
-botoes[j].classList.remove("ativo");
-textos[j].classList.remove("ativo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
@@ -39,4 +12,31 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-12-18");
 
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
